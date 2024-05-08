f6726aff95869e33313f527885e8a2eb1c309383
laiiting <moneyhelen8@gmail.com>
2024-05-08 19:04:48 +0800

    update

--- src/components/layout/layoutHeader.vue
+++ src/components/layout/layoutHeader.vue
@@ -17,8 +17,8 @@ const openLink = (path: string) => {
       <span class="name">強化學習結合深度學習之交易結果</span>
     </div>
     <div class="flex-grow" />
-    <el-menu-item @click="openLink('應用深度學習於金融市場模型_簡報')">簡報</el-menu-item>
     <el-menu-item @click="openLink('應用深度學習於金融市場模型')">企劃書</el-menu-item>
+    <el-menu-item @click="openLink('應用深度學習於金融市場模型_簡報')">簡報</el-menu-item>
     <el-menu-item index="/" v-if="$route.path != '/'">交易結果</el-menu-item>
     <el-menu-item index="author" v-if="$route.path == '/'">作者介紹</el-menu-item>
   </el-menu>
